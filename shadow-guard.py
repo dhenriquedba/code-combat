12f7fce171b0a3af1a6ce567e9d8a55b994b3ef7
Dennis Henrique <dennishenriquedba@gmail.com>
2020-03-01 20:32:35 -0300

    Escape do ogro para pegar as gemas e chegar do outro lado com segurança. Cuidado com os espinhos!

---
+++ shadow-guard.py
@@ -0,0 +1,8 @@
+#Escape do ogro para pegar as gemas e chegar do outro lado com segurança. Cuidado com os espinhos!
+
+# Fique fora da visão do ogro. Colete todas as gemas.
+hero.moveRight()
+hero.moveUp()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
