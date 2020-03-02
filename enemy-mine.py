463d2560b9f5d52a78d491ebb939c6243fbd42ef
Dennis Henrique <dennishenriquedba@gmail.com>
2020-03-01 21:06:40 -0300

    Pise com cuidado, o perigo está no chão.

---
+++ enemy-mine.py
@@ -0,0 +1,8 @@
+#Pise com cuidado, o perigo está no chão.
+
+# Use argumentos nos comandos para mover-se mais.
+hero.moveRight(3)
+hero.moveUp()
+hero.moveRight()
+hero.moveDown(3)
+hero.moveRight(3)
