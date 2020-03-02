92b51b91a7d9e4c23bac4ac1da3af35fd5972199
Dennis Henrique <dennishenriquedba@gmail.com>
2020-03-01 21:12:37 -0300

    Aprenda o nome verdadeiro de um inimigo para derrotá-lo

---
+++ true-names.py
@@ -0,0 +1,10 @@
+#Aprenda o nome verdadeiro de um inimigo para derrotá-lo
+# Defenda-se contra "Brak" e "Treg"!
+# Você precisa atacar os ogros duas vezes.
+
+hero.moveRight()
+hero.attack("Brak")
+hero.attack("Brak")
+hero.moveRight()
+hero.attack("Treg")
+hero.attack("Treg")
