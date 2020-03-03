58c01c041b484537f6f970b287ef805b3184dfec
Dennis Henrique <dennishenriquedba@gmail.com>
2020-03-02 22:53:36 -0300

    Esqueletos: criaturas assustadoras ou medrosas?

---
+++ radiant-aura.py
@@ -0,0 +1,8 @@
+#Esqueletos: criaturas assustadoras ou medrosas?
+
+# Pegue uma Pedra da Luz para que os esqueletos fujam de você por um pequeno intervalo de tempo.
+while True:
+    hero.moveUp()
+    hero.moveDown()
+    hero.moveRight(2)
+    
