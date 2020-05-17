09f8069235a9ea135f949e212631f237d4c63574
Dennis Henrique <dennishenriquedba@gmail.com>
2020-05-17 16:04:20 -0300

    Sobreviva a uma horda de ogros com a orientação de dois heróis experientes!

--- Masmorra-Kithgard/a-mayhem-of-munchins.py
+++ Masmorra-Kithgard/a-mayhem-of-munchins.py
@@ -1,4 +1,4 @@
-#Sobreviva a uma horda de ogros com a orientação de dois heróis experientes!
+#Sobreviva a uma horda de ogros com a orientação de dois heróis experientes! 
 
 # Dentro de um loop while-true, use findNearestEnemy() e ataque!
 while True:
