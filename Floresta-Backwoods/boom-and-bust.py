fdfef8aa89c202c23c86dc34d055521bbfd92cf0
Dennis Henrique <dennishenriquedba@gmail.com>
2020-07-05 15:30:54 -0300

    boom and bust

---
+++ Floresta-Backwoods/boom-and-bust.py
@@ -0,0 +1,9 @@
+#Havia apenas um modo de que a Masmorra Kithgard podia acabar... em chamas! Use cargas de explosivo para demolir os portões e escapar da masmorra!
+ 
+# Use seu martelo buildXY para construir duas armadilhas de fogo (fire-trap) próximas ao portão.
+# Eles irão detonar quando estiver a uma distância segura!
+# E então, corra para a floresta!
+hero.buildXY("fire-trap", 35, 35)
+hero.buildXY("fire-trap", 35, 29)
+hero.moveLeft()
+hero.moveRight(2)
