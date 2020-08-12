cb52b361efa9f53cab62399f15a41c3ea116274c
Dennis Tavares <dennishenriquedba@gmail.com>
2020-08-12 00:08:55 -0300

    Calculando a distância

---
+++ Floresta-Backwoods/range-finder.py
@@ -0,0 +1,24 @@
+#Descubra a distância para os alvos. A artilharia irá fazer o resto! 
+
+# Os ogros estão patrulhando a floresta!
+# Use o metodo distanceTo para encontrar onde os inimigos estão.
+# Informe a distância de cada inimigo, e diga a artilharia onde disparar!
+
+enemy1 = "Gort"
+distance1 = hero.distanceTo(enemy1)
+hero.say(distance1)
+
+enemy2 = "Smasher"
+distance2 = hero.distanceTo(enemy2)
+# Diga a variável distance2!
+hero.say(distance2)
+
+#Não atire nos seus amigos!
+friend3 = "Charles"
+hero.say("Hello my friend!")
+
+# The last ogre.
+enemy4 = "Gorgnub"
+# Encontre e diga a distância do resto dos inimigos:
+distance3 = hero.distanceTo(enemy4)
+hero.say(enemy4)
